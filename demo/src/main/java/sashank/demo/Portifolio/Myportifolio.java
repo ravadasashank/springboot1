412ad2f58da2b90e18f289b942a85af97375fbe4
ravadasashank <ravadasashank444@gmail.com>
2026-07-14 12:40:45 +0530

    PostMan

--- demo/src/main/java/sashank/demo/Portifolio/Myportifolio.java
+++ demo/src/main/java/sashank/demo/Portifolio/Myportifolio.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Myportifolio {
-    @GetMapping("/myself")
+    @GetMapping("/")
     public String Myself(){
         return """
                 <h1>Myself</h1>
--- demo/src/main/java/sashank/demo/Studentserver/Studentserver.java
+++ demo/src/main/java/sashank/demo/Studentserver/Studentserver.java
@@ -1,8 +1,26 @@
 package sashank.demo.Studentserver;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Studentserver {
+    //1. Store the Student Details -- POST
+    @PostMapping("/create")
+    public String StoreStudent(){
+        return """
+                id: 1
+                Name: Sashank
+                Dept: CSE
+                age: 21
+                """;
+    }
 
+    //2. Read the Details -- GET
+
+
+    //3. Update the Details -- PUT/PATCH
+
+
+    //4.Delete the Detalis -- DELETE
 }
